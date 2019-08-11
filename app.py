f758ecd695e5d582853f6ef945a183827f0049ca
Clovis Mugaruka <clovis.mugaruka@gmail.com>
2019-08-11 15:26:08 +0300

    updated

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ you don't always need a fully fledged database like sqlite, MysQL, etc.
 """
 
 import gspread
+import pprint
 from oauth2client.service_account import ServiceAccountCredentials
 
 # define scope, and authorize client
@@ -19,4 +20,6 @@ client = gspread.authorize(credentials)
 sheet = client.open('Programming Books').sheet1
 # retrieve all records
 records = sheet.get_all_records()
-print(records)
+# some pretty print magic
+pp = pprint.PrettyPrinter(indent=4, compact=True)
+pp.pprint(records)
